143fc7270c5dc4538d5b91a4c1929ce89e8f9d0c
matt manes <mattmanes@pm.me>
2023-01-21 19:07:08 -0600

    Added test template and changed naming to be PEP compliant.

---
+++ tests/test_printbuddies.py
@@ -0,0 +1,39 @@
+import pytest
+
+import printbuddies
+
+
+def test_printbuddies_clear():
+    ...
+
+
+def test_printbuddies_print_in_place():
+    ...
+
+
+def test_printbuddies_ticker():
+    ...
+
+
+def test_printbuddies_reset():
+    ...
+
+
+def test_printbuddies_get_percent():
+    ...
+
+
+def test_printbuddies__prepare_bar():
+    ...
+
+
+def test_printbuddies__trim_bar():
+    ...
+
+
+def test_printbuddies_get_bar():
+    ...
+
+
+def test_printbuddies_display():
+    ...
