568499d8d99b52fd439747075df3babc1ca48fba
matt manes <mattmanes@pm.me>
2023-04-25 13:53:04 -0500

    tests: update tests

--- tests/test_printbuddies.py
+++ tests/test_printbuddies.py
@@ -8,7 +8,7 @@ import printbuddies
 def test_printbuddies_display():
     print()
     total = 100
-    bar = printbuddies.ProgBar(total - 1)
+    bar = printbuddies.ProgBar(total)
     for _ in range(total):
         bar.display()
         time.sleep(0.01)
@@ -17,3 +17,10 @@ def test_printbuddies_display():
     for _ in range(total):
         bar.display()
         time.sleep(0.01)
+    bar.reset()
+    bar.update_frequency = 1
+    for i in range(total):
+        bar.display(counter_override=i)
+        time.sleep(0.01)
+    bar.reset()
+    [bar.display(return_object=time.sleep(0.01)) for _ in range(total)]
