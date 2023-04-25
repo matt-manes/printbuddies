227d6d89f5bb75f3cc006d30211242419013d6bc
matt manes <mattmanes@pm.me>
2023-04-25 14:18:20 -0500

    tests: update tests

--- tests/test_printbuddies.py
+++ tests/test_printbuddies.py
@@ -20,7 +20,7 @@ def test_printbuddies_display():
     bar.reset()
     bar.update_frequency = 1
     for i in range(total):
-        bar.display(counter_override=i)
+        bar.display(counter_override=i + 1)
         time.sleep(0.01)
     bar.reset()
     [bar.display(return_object=time.sleep(0.01)) for _ in range(total)]
