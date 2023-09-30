f7ece81a2d579c5e05b29bf58d87b391702dd31a
matt manes <mattmanes@pm.me>
2023-09-29 20:06:48 -0500

    chore: build v1.4.1

--- src/printbuddies/__init__.py
+++ src/printbuddies/__init__.py
@@ -1,3 +1,4 @@
-from .printbuddies import PoolBar, ProgBar, Spinner, clear, print_in_place, ticker
+from .printbuddies import (PoolBar, ProgBar, Spinner, clear, print_in_place,
+                           ticker)
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
